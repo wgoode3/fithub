e2c349194976bb7f1a957aac9c7641a8a71455e1
William Goode <wgoode3@gmail.com>
2020-12-28 06:10:00 -0500

    playing with shell

--- start-dev.sh
+++ start-dev.sh
@@ -1,26 +1,44 @@
 # this all assumes we're running it in Linux / MacOS
 
-StartServer () {
-    echo "Activating Virtual Environment"
-    source server/venv/bin/activate
-    echo "Starting Back-End"
-    python3 server/manage.py runserver
+ActivateVirtualEnv () {
+    if [ ! -d server/venv ]; then
+        echo "creating virtualenvironment"
+        python3 -m venv server/venv
+        source server/venv/bin/activate
+        echo "installing back-end dependencies"
+        echo "this may take a few seconds..."
+        pip install -r requirements.txt
+    else
+        echo "activating virtualenv"
+        source server/venv/bin/activate
+    fi
 }
 
-StartClient() {
+StartServer () {
+    echo "Starting Back-End"
+    python server/manage.py runserver
+}
+
+StartClient () {
     cd client
     echo "Starting Front-End"
-    npm start
+    # npm start
+    # prevent's clearing console?
+    # https://github.com/facebook/create-react-app/issues/7640
+    COLOR=1 npm start | cat
 }
 
+# start by activating the virtual environment
+
+ActivateVirtualEnv
+
+# next run both the front-end and back-end at the same time
 # solution to start both concurrently from 
 # https://stackoverflow.com/questions/3004811/how-do-you-run-multiple-programs-in-parallel-from-a-bash-script#answer-41762802
+
 StartServer &
 P1=$!
 StartClient &
 P2=$!
 
 wait $P! $P2
-
-echo "shutting down"
-cd ..
