1dc7a304b52599dc586576b01d1128cff1bd7f4b
William Goode <wgoode3@gmail.com>
2020-12-28 06:22:37 -0500

    fixed the shell script some more

--- start-dev.sh
+++ start-dev.sh
@@ -22,10 +22,11 @@ StartServer () {
 StartClient () {
     cd client
     echo "Starting Front-End"
-    # npm start
-    # prevent's clearing console?
+    # this prevents clearing console
+    # solution found here:
     # https://github.com/facebook/create-react-app/issues/7640
     COLOR=1 npm start | cat
+    # looks like it pipes the output text of `npm start` through `cat`
 }
 
 # start by activating the virtual environment
@@ -36,9 +37,19 @@ ActivateVirtualEnv
 # solution to start both concurrently from 
 # https://stackoverflow.com/questions/3004811/how-do-you-run-multiple-programs-in-parallel-from-a-bash-script#answer-41762802
 
+DelayedMessage() {
+    sleep 5
+    # maybe 5 is enough of a delay?
+    # there is probably a better way to make this print
+    # after the react server starts
+    echo "Use ctrl + c to stop both servers..."
+}
+
 StartServer &
 P1=$!
 StartClient &
 P2=$!
 
-wait $P! $P2
+DelayedMessage
+
+wait $P1 $P2
